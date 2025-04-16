85250e2e9aa5db01e86071b186dc4bb6565cf74a
mag215 <blr.artyom.gonchar@gmail.com>
2025-04-16 20:49:18 +0300

    add teacher view, courses view

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('courses/', views.courses, name='courses'),
+    path('teachers/', views.teachers_list, name='teachers_list'),
+    path('courses/<int:pk>/', views.course_detail, name='course-detail'),
 ]
